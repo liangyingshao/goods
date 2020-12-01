203ab36719b952645d9c680ceb22b682ffcc4d13
Sharon Qin <50222604+gyanlala@users.noreply.github.com>
2020-12-01 19:54:03 +0800

    Revert "Merge branch 'ym' into backUp1"
    This reverts commit 67d585d15d0dbd59e164301ca990098356ea731a, reversing
    changes made to ac4630c8f9ddb30e8db08fd128b0ed459033f12c.

--- goodsService/src/test/java/cn/edu/xmu/goods/controller/GoodsControllerTest.java
+++ goodsService/src/test/java/cn/edu/xmu/goods/controller/GoodsControllerTest.java
@@ -3,7 +3,6 @@ package cn.edu.xmu.goods.controller;
 import cn.edu.xmu.goods.GoodsServiceApplication;
 import cn.edu.xmu.ooad.util.JwtHelper;
 import cn.edu.xmu.ooad.util.ResponseCode;
-import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -156,49 +155,10 @@ class GoodsControllerTest {
         JSONAssert.assertEquals(expectedResponse,responseString,true);
     }
 
-    @Test
-    public void addSkuComment1() throws Exception{
-        String token = creatTestToken(1L, 0L, 100);
-        String responseString = this.mvc.perform(post("/goods/orderitems/1/comments").header("authorization",token).contentType("application/json;charset=UTF-8").queryParam("type", "0").queryParam("content", "购物体验良好"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"))
-                .andReturn().getResponse().getContentAsString();
-
-        String expectedResponse = "{\"errno\": 0, \"errmsg\": \"成功\"}";
-        JSONAssert.assertEquals(expectedResponse, responseString, true);
-        //一要检测返回值是否符合预期
-        //二要检查数据库的值是否符合预期
-    }
-
-    @Test
-    public void getcommentState1() throws Exception{
-        String token = creatTestToken(1L, 0L, 100);
-        String responseString = this.mvc.perform(get("/goods/comments/states").header("authorization",token).contentType("application/json;charset=UTF-8"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"))
-                .andReturn().getResponse().getContentAsString();
-        String expectedResponse="{\"errno\":0,\"data\":[{\"name\":\"待审核\",\"code\":0},{\"name\":\"审核通过\",\"code\":1},{\"name\":\"审核不通过\",\"code\":2}],\"errmsg\":\"成功\"}";
-        JSONAssert.assertEquals(expectedResponse,responseString,true);
-    }
-
-    @Test
-    public void selectAllPassComment1() {
-        String responseString = null;
-        String token = creatTestToken(1L, 0L, 100);
-        try {
-            responseString = this.mvc.perform(get("/goods/skus/1/comments?page=1&pageSize=2").header("authorization", token))
-                    .andExpect(status().isOk())
-                    .andExpect(content().contentType("application/json;charset=UTF-8"))
-                    .andReturn().getResponse().getContentAsString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String expectedResponse = "{\"errno\":0,\"data\":{\"total\":0,\"pages\":0,\"pageSize\":0,\"page\":1,\"list\":[]},\"errmsg\":\"成功\"}";
-        try {
-            JSONAssert.assertEquals(expectedResponse, responseString, false);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
+//    @Test
+//    void uploadSkuImg()  throws Exception
+//    {
+//        String token = creatTestToken(1L, 0L, 100);
+//        String responseString=this.mvc.perform(post("/goods/shops/0/skus/273/uploadImg").header("authorization",token).)
+//    }
 }
