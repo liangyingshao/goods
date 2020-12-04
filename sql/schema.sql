6f94b2e9ca915a0ea519e6e303eee81f4f652aea
xuqingyun <2289105194@qq.com>
2020-12-05 00:09:08 +0800

    更新数据库(增加UNIQUE索引)

--- sql/schema.sql
+++ sql/schema.sql
@@ -29,6 +29,7 @@ CREATE TABLE `brand` (
   `image_url` varchar(255) DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
+  UNIQUE KEY `brand_name_uindex` (`name`),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -72,6 +73,7 @@ CREATE TABLE `coupon` (
   `state` tinyint DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
+  UNIQUE KEY `coupon_sn_uindex` (`coupon_sn`),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -193,6 +195,7 @@ CREATE TABLE `goods_category` (
   `pid` bigint DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
+  UNIQUE KEY `goods_category_name_uindex` (`name`),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=140 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -218,6 +221,8 @@ CREATE TABLE `goods_sku` (
   `disabled` tinyint DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
+  UNIQUE KEY `sku_sn_uindex` (`sku_sn`),
+  UNIQUE KEY `spu_configuration_uindex` (`goods_spu_id`,`configuration`),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=680 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -244,6 +249,7 @@ CREATE TABLE `goods_spu` (
   `disabled` tinyint DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
+  UNIQUE KEY `goods_sn_uindex` (`goods_sn`),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=680 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
